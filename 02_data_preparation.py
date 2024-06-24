441b9e8423cbd985f4a5d5777219519f31b7e649
Is Faid <faid.aznam@gmail.com>
2024-06-24 12:56:42 +0800

    refactor output

--- 02_data_preparation.py
+++ 02_data_preparation.py
@@ -1,9 +1,14 @@
+from processes.data_sampling import prepare_train_test_data
 from processes.preprocess_data import read_data, prepare_data
 from processes.statistical_analysis import print_stats
 
+
 if __name__=="__main__":
     try:
+        print("Starting Data preparation...")
+        print("Accessing Data...")
         raw_data = read_data()
+        print("Performing Data preparations...")
         processed_data = prepare_data(raw_data)
         print_stats(processed_data)
     except ValueError as e:
