c051e44f70cfde9ed807984a81486878b543bfe0
Is Faid <faid.aznam@gmail.com>
2024-06-24 12:56:10 +0800

    refactor output

--- 01_data_analysis.py
+++ 01_data_analysis.py
@@ -4,10 +4,11 @@ from processes.preprocess_data import read_data
 
 if __name__=="__main__":
     try:
+        print("Starting Data analysis...")
         print("Accessing Data...")
         raw_data = read_data()
         print("Generating Data Visuals...")
         generate_data_visual(raw_data)
-        print("01_data_analysis.py successfully run...")
+        print("Data analysis completed.")
     except ValueError as e:
         raise ValueError(f"Unexpected error occurred: {e}") from e
