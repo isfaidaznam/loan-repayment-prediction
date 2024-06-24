a245b20b1e2b33d7926c2535c776b659bb83a580
Is Faid <faid.aznam@gmail.com>
2024-06-24 14:36:06 +0800

    Implement model prediction

--- config/config.py
+++ config/config.py
@@ -4,3 +4,8 @@ class Config:
     def __init__(self, config_file='config/config.yaml'):
         with open(config_file, 'r') as f:
             self.__dict__ = yaml.safe_load(f)
+
+class Config_Predict:
+    def __init__(self, config_file='ann_model/config.yaml'):
+        with open(config_file, 'r') as f:
+            self.__dict__ = yaml.safe_load(f)
